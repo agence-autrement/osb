ffcd389284a2c84c656ead2200485ecaba577745
Clément <MaxiProd@autrement3.home>
2017-05-26 17:36:37 +0200

    Création page Mécénat.

---
+++ wp-content/themes/osb/page-mecenat.php
@@ -0,0 +1,43 @@
+<?
+/*
+Template Name: Page Mécénat
+*/
+?>
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+<div id="content">
+    <section class="top_page_mecenat">
+        <div class="contenu_grid">
+            <div class="title">
+                Concerto, <span>le cercle des amis de l'OSB</span>
+            </div>
+            <div class="btn_savoir_plus"><a href="###">En savoir +</a></div>
+            <div class="intreo_text">
+                Spectateur(trice) de l'Orchestre Symphonique de Bretagne, vous souhaitez apporter votre soutien aux activités de l'OSB et bénéficier d'avantages exclusifs ? Rejoignez Concerto, le Cercle des Amis de l'OSB !
+            </div>
+        </div>
+    </section>
+    <section class="pourquoi_rejoindre">
+        <div class="contenu_grid">
+            <div class="rejoindre">
+                <div class="titre_pourquoi">
+                    Pourquoi nous rejoindre ?
+                </div>
+                <div class="txt_pourquoi">
+                    Concerto vous propose une relation privilégiée avec l’OSB, et rassemble des membres qui encouragent la démarche citoyenne de l’Orchestre.
+                </div>
+            </div>
+            <div class="rejoindre">
+                <div class="titre_pourquoi">
+                    À quoi sert votre adhésion ?
+                </div>
+                <div class="txt_pourquoi">
+                    L’OSB a besoin de vous pour soutenir deux projets phares de la saison 2017-2018 : + les activités de la Fabrik, le service dédié à l’éducation artistique et culturelle ; + la résidence artistique d’Anne Gastinel. Vous serez informé(e) par nos équipes de l’utilisation des fonds. Par exemple, 500€ récoltés = 1 intervention musicale par 5 musiciens dans une école, un hôpital ou un centre pénitentiaire.
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php get_footer(); ?>
