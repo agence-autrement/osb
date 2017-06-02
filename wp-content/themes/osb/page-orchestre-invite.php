3953203b74139ad88e811e631572c0b4ca522e65
Clément <MaxiProd@autrement3.home>
2017-06-02 11:19:21 +0200

    Page saison Piccolo // Clean css

--- wp-content/themes/osb/page-orchestre-invite.php
+++ wp-content/themes/osb/page-orchestre-invite.php
@@ -342,57 +342,6 @@ Template Name: Page Orchestre Invité
         </div>
     </section>
     <? get_template_part('joinUs'); ?>
-    <section class="suggestion">
-        <div class="contenu_grid">
-            <div class="titre_sugg">// Vous aimerez peut-être //</div>
-            <ul>
-                <li>
-                    <div class="left_event">
-                        <p class="cat_calendrier">Les essentiels</p>
-                        <p class="titre_calendrier">Titre Event</p>
-                        <p class="artiste_calendrier">Nom de l'artiste</p>
-                        <a href="###" class="link_calendrier">EN SAVOIR +</a>
-                    </div>
-                    <div class="right_event">
-                        <p class="day_calendrier">
-                            29
-                        </p>
-                        <p class="month_calendrier">Juin</p>
-                        <p class="lieu_calendrier">Rennes</p>
-                    </div>
-                </li>
-                <li>
-                    <div class="left_event">
-                        <p class="cat_calendrier">Les essentiels</p>
-                        <p class="titre_calendrier">Titre Event</p>
-                        <p class="artiste_calendrier">Nom de l'artiste</p>
-                        <a href="###" class="link_calendrier">EN SAVOIR +</a>
-                    </div>
-                    <div class="right_event">
-                        <p class="day_calendrier">
-                            29
-                        </p>
-                        <p class="month_calendrier">Juin</p>
-                        <p class="lieu_calendrier">Rennes</p>
-                    </div>
-                </li>
-                <li>
-                    <div class="left_event">
-                        <p class="cat_calendrier">Les essentiels</p>
-                        <p class="titre_calendrier">Titre Event</p>
-                        <p class="artiste_calendrier">Nom de l'artiste</p>
-                        <a href="###" class="link_calendrier">EN SAVOIR +</a>
-                    </div>
-                    <div class="right_event">
-                        <p class="day_calendrier">
-                            29
-                        </p>
-                        <p class="month_calendrier">Juin</p>
-                        <p class="lieu_calendrier">Rennes</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </section>
+    <? get_template_part('vous-aimerez') ?>
 </div>
 <?php get_footer(); ?>
