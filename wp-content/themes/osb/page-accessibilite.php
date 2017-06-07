17464c8899555267225d4faa676d8b5ca15b8141
Agence-Autrement <romaric@autrement.bzh>
2017-06-07 11:10:24 +0200

    Accessibilité

---
+++ wp-content/themes/osb/page-accessibilite.php
@@ -0,0 +1,114 @@
+<?
+/*
+Template Name: Page Accessibilite
+*/
+?>
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+<div id="content">
+    <section class="section-header-ressources section-header-access">
+
+        <div class="contenu_grid">
+
+
+            <div class="header-ressources__inner">
+                <h1 class="header-ressources__title">Accessibilité <span>+</span> handicap</h1>
+
+                <div class="header-ressources__content">
+                    <a href="#" class="header-ressources__btn btn">Culture Justice</a><span>+</span>
+                    <a href="#" class="header-ressources__btn btn">Culture santé</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-access">
+        <div class="contenu_grid">
+
+            <div class="access__inner">
+                <div class="access--left">
+
+                    <div class="access--left__inner">
+                        <h2 class="access__name">Accessibilité</h2>
+                        <p class="access__text">
+                            L’ensemble des concerts de l’OSB sont accessibles aux personnes à mobilité réduite et aux
+                            personnes déﬁcientes visuelles.
+                        </p>
+                        <p class="access__text access__text--blue">
+                            Renseignements et réservations auprès de la billetterie de l’orchestre au 02 99 275 275 ou
+                            <a href="mailto:billetterie@o-s-b.fr">billetterie@o-s-b.fr</a>
+                        </p>
+                        <p class="access__text">
+                            Informations complémentaires dans le guide de l’accessibilité à la vie culturelle
+                            rennaise.</p>
+
+
+                    </div>
+                </div>
+
+                <div class="access--right">
+
+                    <div class="access--right__inner">
+                        <h2 class="access__name">Handicap</h2>
+                        <p class="access__text">
+                            Un parcours de concerts et de rencontres avec des artistes invités est proposé chaque saison
+                            au public déﬁcient visuel, à tarif préférentiel. Une présentation de ces rendez-vous
+                            accessibles sera proposée le mardi 19 septembre, de 17h30 à 19h30 aux Champs Libres. </p>
+                        <p class="access__text access__text--blue access__text--blue--right">
+                            Renseignements et réservations :<br> <a
+                                href="mailto:lafabrik@o-s-b.fr">lafabrik@o-s-b.fr</a>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-access-bg">
+
+    </section>
+
+    <section class="section-culture">
+        <div class="contenu_grid">
+
+            <div class="culture__inner">
+                <div class="culture--left">
+
+                    <div class="culture--left__inner">
+                        <h2 class="culture__name">Culture Justice</h2>
+                        <p class="culture__text">
+                            L’OSB est membre du dispositif « Sound From » autour duquel plusieurs structures musicales
+                            rennaises (Les Transmusicales, le Jardin Moderne, l’Antipode) et la Ligue de l’Enseignement
+                            se sont associées pour proposer une programmation riche et variée aux personnes en
+                            détention. Des moments d’échange et de partage qui vont dans le sens du droit à la culture
+                            pour tous.
+                        </p>
+                        <p class="culture__text">
+                            L’Orchestre Symphonique de Bretagne donnera un concert symphonique autour de Vivaldi le
+                            vendredi 25 août 2017 au Centre Pénitentiaire de Rennes Vezin.</p>
+
+
+                    </div>
+                </div>
+
+                <div class="culture--right">
+
+                    <div class="culture--right__inner">
+                        <h2 class="culture__name">Culture santé</h2>
+                        <p class="culture__text">
+                            L’Orchestre est chaque saison partenaire de nombreux établissements de santé et construit
+                            des parcours musicaux adaptés à chacun. La clinique Saint-Laurent, Guillaume Régnier,
+                            l’accueil de jour Kerelys, le Pôle Saint-Hélier participent régulièrement aux actions
+                            proposées par l’orchestre.</p>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+<?php get_footer(); ?>
