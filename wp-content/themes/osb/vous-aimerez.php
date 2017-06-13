3e762d2a05b0fd55ebac5c15fc897716c1214977
Clément <MaxiProd@autrement3.home>
2017-06-13 11:47:40 +0200

    Uploads

--- wp-content/themes/osb/vous-aimerez.php
+++ wp-content/themes/osb/vous-aimerez.php
@@ -7,52 +7,6 @@
 <section class="suggestion">
     <div class="contenu_grid">
         <div class="titre_sugg">// Vous aimerez peut-être //</div>
-        <ul>
-            <li>
-                <div class="left_event">
-                    <p class="cat_calendrier">Les essentiels</p>
-                    <p class="titre_calendrier">Titre Event</p>
-                    <p class="artiste_calendrier">Nom de l'artiste</p>
-                    <a href="###" class="link_calendrier">EN SAVOIR +</a>
-                </div>
-                <div class="right_event">
-                    <p class="day_calendrier">
-                        29
-                    </p>
-                    <p class="month_calendrier">Juin</p>
-                    <p class="lieu_calendrier">Rennes</p>
-                </div>
-            </li>
-            <li>
-                <div class="left_event">
-                    <p class="cat_calendrier">Les essentiels</p>
-                    <p class="titre_calendrier">Titre Event</p>
-                    <p class="artiste_calendrier">Nom de l'artiste</p>
-                    <a href="###" class="link_calendrier">EN SAVOIR +</a>
-                </div>
-                <div class="right_event">
-                    <p class="day_calendrier">
-                        29
-                    </p>
-                    <p class="month_calendrier">Juin</p>
-                    <p class="lieu_calendrier">Rennes</p>
-                </div>
-            </li>
-            <li>
-                <div class="left_event">
-                    <p class="cat_calendrier">Les essentiels</p>
-                    <p class="titre_calendrier">Titre Event</p>
-                    <p class="artiste_calendrier">Nom de l'artiste</p>
-                    <a href="###" class="link_calendrier">EN SAVOIR +</a>
-                </div>
-                <div class="right_event">
-                    <p class="day_calendrier">
-                        29
-                    </p>
-                    <p class="month_calendrier">Juin</p>
-                    <p class="lieu_calendrier">Rennes</p>
-                </div>
-            </li>
-        </ul>
+        <? resultDateDefault(); ?>
     </div>
 </section>
